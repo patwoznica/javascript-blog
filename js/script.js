17d1beaeae2f5e0a0e9d3d90eae74e05d76bea82
Patryk Woznica <pat.woznica@gmail.com>
2021-08-05 21:48:07 +0200

    Adding lines from ex. 5.4

--- js/script.js
+++ js/script.js
@@ -40,8 +40,58 @@ targetArticle.classList.add('active');
     
 }
 
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+    
+const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML='';   
+
+  /* for each article */
+    
+const articles = document.querySelectorAll(optArticleSelector);
+    
+let html = '';
+    
+for(let article of articles){
+    
+    /* get the article id */
+    
+const articleId = article.getAttribute('id');
+    console.log(articleId);
+    
+    /* find the title element */
+    
+const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log(articleTitle);
+    
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    
+const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+        
+    /* insert link into titleList */
+    
+html = html + linkHTML;
+console.log(html);
+         
+}
+    
+titleList.innerHTML = html;
+    
 const links = document.querySelectorAll ('.titles a');
+console.log(links);
 
 for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+    link.addEventListener('click' , titleClickHandler);
 }
+}
+    
+generateTitleLinks();
+
